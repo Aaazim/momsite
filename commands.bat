31b3fb087616b2fae829a235453df77006db59fe
Mohammad Aazim <aazimm@ymail.com>
2014-03-04 02:06:14 +0530

    Added all the necessary files for index page.
    All css and images are now added, No text changes yet

---
+++ commands.bat
@@ -0,0 +1,3 @@
+
+cd C:\Users\Vaio\Documents\GitHub
+"C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" --port=10000 --admin_port=9999 momsite
---
+++ views.py
@@ -0,0 +1,22 @@
+import os
+import jinja2
+import webapp2
+
+# TODO: move em to settings.
+DIRNAME = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(DIRNAME))
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'greetings': 'foo',
+            'url': 'foo',
+            'url_linktext': '',
+        }
+
+        template = jinja_environment.get_template('mom.html')
+        self.response.out.write(template.render(template_values))
+
+
