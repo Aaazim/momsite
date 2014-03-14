500d48cf482de83577633d7b94594d6652dc1bd6
Mohammad Aazim <aazimm@ymail.com>
2014-03-15 04:36:44 +0530

    15th march 2014
    Whats left-- The online pdf documents in the "Services" !

--- commands.bat
+++ commands.bat
@@ -1,3 +1,3 @@
 
 cd C:\Users\Vaio\Documents\GitHub
-"C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" --port=10000 --admin_port=9999 momsite
+C:\Python27\python.exe "C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" --port=10000 --admin_port=9999 momsite
