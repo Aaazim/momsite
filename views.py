6c3f2142436f9cb578abd48bcb8745a7b94eaf65
Mohammad Rafi <confessin@gmail.com>
2014-03-25 00:11:34 +0530

    Added email support.

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 import os
 import jinja2
 import webapp2
+from google.appengine.api import mail
 
 # TODO: move em to settings.
 DIRNAME = os.path.join(os.path.dirname(__file__), 'templates')
@@ -24,8 +25,27 @@ class MainPage(webapp2.RequestHandler):
             "foo":self.request.POST['name'],   
             'message': 'Thanks for your response, We will get back to you.',    
         }
-        print "foo"
-        
+        send_mail(self.request)
+        send_confirmation_mail(self.request)
         template = jinja_environment.get_template('mom.html')
         self.response.out.write(template.render(template_values))
 
+
+
+def send_mail(request):
+    body= 'Subject: %s \n %s' %(request.POST['subject'], request.POST['message'])
+    subject="Update on your site dr-ishrat: by %s [%s]" %(request.POST['name'], request.POST['email'])  
+    mail.send_mail(sender="Mohammad Aazim <warcode91@gmail.com>",
+                  to="Mohammad Aazim <warcode91@gmail.com>, Ishrat Alim <ishrat_alim@yahoo.com",
+                  subject=subject,
+                  body=body)
+    return
+
+def send_confirmation_mail(request):
+    body= 'Thanks for reaching to us, We will get back to you shortly.'
+    subject='Message receipt confirmation from dr-ishrat'
+    mail.send_mail(sender="Admin [dr-ishrat] <warcode91@gmail.com>",
+                  to=request.POST['email'],
+                  subject=subject,
+                  body=body)
+    return
